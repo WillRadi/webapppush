33ab1795012467792f066ed648fcfc183e96a20e
Will Radi <williamradi@gmail.com>
2024-10-28 23:26:08 -0300

    feat: add app.js no rolê sem quebrar

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import React, { useEffect } from "react";
 import { onMessage } from "firebase/messaging";
 import { messaging } from "./firebase";
-import { requestPermission } from "./requestPermission"
+import { getToken } from "firebase/messaging";
 
 function App() {
   function test() {
@@ -28,12 +28,28 @@ function App() {
     });
   }, []);
 
+  const requestPermission = async () => {
+    try {
+      const token = await getToken(messaging, {
+        vapidKey: "BL78wBZ7tj5H4X_lEjlUDvutAG6dyfWQy8oEYCmrywGvtWFRpqA1Vbspfo6e6d8JfTlHLsMgACdGQ5hqhIiILb4"
+      });
+      if (token) {
+        console.log("Token de notificação:", token);
+        // Armazene o token no backend, se necessário
+      } else {
+        console.log("Não foi possível obter o token.");
+      }
+    } catch (error) {
+      console.error("Erro ao obter o token:", error);
+    }
+  };
+
   return (
     <div className="App">
       {test()}
+      {requestPermission()}
       {console.log('outro teste')}
       <h1>Carregou o App.js</h1>
-      <requestPermission />
     </div>
   );
 }
