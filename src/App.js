d7d8492f2b28822843bf750d387b7682426fd211
Will Radi <williamradi@gmail.com>
2024-10-28 23:30:59 -0300

    fix app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 
 import React, { useEffect } from "react";
-import { onMessage } from "firebase/messaging";
+import { onMessage, getToken } from "firebase/messaging";
 import { messaging } from "./firebase";
-import { getToken } from "firebase/messaging";
 
 function App() {
   function test() {
-    console.log('passou no teste')
+    console.log('passou no teste');
   }
 
+  // Registrar o Service Worker
   if ("serviceWorker" in navigator) {
     navigator.serviceWorker
       .register("/firebase-messaging-sw.js")
@@ -22,33 +22,35 @@ function App() {
   }
 
   useEffect(() => {
+    // Configurar recebimento de mensagens em primeiro plano
     onMessage(messaging, (payload) => {
       console.log("Mensagem recebida no app:", payload);
       // Exibir notificação ou atualizar o estado do app
     });
-  }, []);
 
-  const requestPermission = async () => {
-    try {
-      const token = await getToken(messaging, {
-        vapidKey: "BL78wBZ7tj5H4X_lEjlUDvutAG6dyfWQy8oEYCmrywGvtWFRpqA1Vbspfo6e6d8JfTlHLsMgACdGQ5hqhIiILb4"
-      });
-      if (token) {
-        console.log("Token de notificação:", token);
-        // Armazene o token no backend, se necessário
-      } else {
-        console.log("Não foi possível obter o token.");
+    // Solicitar permissão para notificações
+    const requestPermission = async () => {
+      try {
+        const token = await getToken(messaging, {
+          vapidKey: "BL78wBZ7tj5H4X_lEjlUDvutAG6dyfWQy8oEYCmrywGvtWFRpqA1Vbspfo6e6d8JfTlHLsMgACdGQ5hqhIiILb4"
+        });
+        if (token) {
+          console.log("Token de notificação:", token);
+          // Armazene o token no backend, se necessário
+        } else {
+          console.log("Não foi possível obter o token.");
+        }
+      } catch (error) {
+        console.error("Erro ao obter o token:", error);
       }
-    } catch (error) {
-      console.error("Erro ao obter o token:", error);
-    }
-  };
+    };
+
+    requestPermission();
+  }, []);
 
   return (
     <div className="App">
       {test()}
-      {requestPermission()}
-      {console.log('outro teste')}
       <h1>Carregou o App.js</h1>
     </div>
   );
